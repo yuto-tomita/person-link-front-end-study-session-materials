cb28e220899fc8a5b9ba5a3e0886652a2d3e3da0
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-08-07 01:15:42 +0900

    Slide作成し、一旦コミット

---
+++ components/slide/SlideOne.vue
@@ -0,0 +1,6 @@
+<template>
+	<h1>Personlink Frontend Study</h1>
+	<div class="text-slate-500">
+		Vue3とTypeScriptの雰囲気を知ろう
+	</div>
+</template>
---
+++ components/slide/SlideThree.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="mt-10 text-lg text-slate-800">
+    MicroSoftによって開発されているプログラミング言語の一つ。
+    <br>
+    AltJS(Alternative Javascript)の一つです。
+  </div>
+
+  <div class="mt-4">
+    AltJSはJavaScriptの代替言語です。
+    <br>
+    現在使用されているのはTypeScriptが主流になっていますがが、CoffeeScriptやHaxe, Dartなどが存在しています。
+  </div>
+
+  <div class="mt-4">
+    TypeScriptの用途はJavaScriptと同じであり、サーバーサイド、フロントエンドと幅広く使うことができます。
+  </div>
+
+  <div class="mt-6">
+    JavaScriptと違う点は、TypeScriptにはtsc(typescript compiler)を備えていて、プログラムによる型チェックが行われるようになります。
+    型チェックが行われることによって、型に関連されるプログラムはコンパイルエラーとして検出されるようになります。
+  </div>
+</template>
