526fe3d83cda247ac41ff05a16ae2e23eab08745
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-08-09 11:23:31 +0900

    slideをコンポーネントで管理をやめたため削除

--- components/slide/SlideOne.vue
+++
@@ -1,6 +0,0 @@
-<template>
-	<h1>Personlink Frontend Study</h1>
-	<div class="text-slate-500">
-		Vue3とTypeScriptの雰囲気を知ろう
-	</div>
-</template>
--- components/slide/SlideTwo.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <ul class="ml-10 mt-10">
-    <li>TypeScriptとは？</li>
-    <li>TypeScriptでの型定義の仕方</li>
-    <li>Vue3 + TSで作成したTODOアプリの説明</li>
-    <li>Vue3 + TSでの実装コードを見る</li>
-    <li>Vueを使う上でのTips</li>
-  </ul>
-</template>
